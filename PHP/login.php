eb4baf5bccc53d8945277804eca1bde2e7e5b682
Daniel <danielmonr@gmail.com>
2016-11-08 16:33:49 -0600

    mas errores

--- PHP/login.php
+++ PHP/login.php
@@ -2,9 +2,9 @@
   session_start();
   $_SESSION['logged'] = 0;
 
-  if (isset($_POST['submit'])) {
+  if (isset($_POST['submit'])) 
     if ($_POST['username'] == "mike@m.com" &&
         $_POST['password'] == "1234")
       $_SESSION['logged'] = 1;
     echo "Login<br>";
-?>
+  ?>
