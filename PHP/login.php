04057b41bea4fced35342ca88fa3c134f6d138c9
sbaidachni <mikedm195@gmail.com>
2016-11-08 15:55:53 -0600

    log in

---
+++ PHP/login.php
@@ -0,0 +1,10 @@
+<?php
+  session_start();
+  $_SESSION['logged'] = 0;
+
+  if (isset($_POST['submit'])) {
+    if ($_POST['username'] == "mike@m.com" &&
+        $_POST['password'] == "1234") {
+      $_SESSION['logged'] = 1;
+    echo "Login<br>";      
+?>
