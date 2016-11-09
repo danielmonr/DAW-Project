b6c4daf35f3a99e1c76dbaae57bc721f62ac243d
Daniel <danielmonr@gmail.com>
2016-11-08 19:10:49 -0600

    cambios a login

--- PHP/login.php
+++ PHP/login.php
@@ -3,55 +3,41 @@
    $db = new DBConnection();
    $dbc = $db->Connect();
    session_start();
-   
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
+
+   $valid = false;
+
+   if (isset($_POST['username']) && isset($_POST['password'])){
+     $valid = true;
+   }
+   else{
+     print ("Please insert your username and password.<br/>");
+   }
+
+   $myusername = mysqli_real_escape_string($dbc,$_POST['username']);
+   $mypassword = mysqli_real_escape_string($dbc,$_POST['password']);
+   $sql = "SELECT * FROM Users WHERE email = $myusername' and password = '$mypassword'";
+   $result = mysqli_query($dbc, $sql);
+
+   print ($result);
+
+
+   /*if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form
+
       $myusername = mysqli_real_escape_string($dbc,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($dbc,$_POST['password']); 
-      
+      $mypassword = mysqli_real_escape_string($dbc,$_POST['password']);
+
       $sql = "SELECT id FROM Users WHERE email = $myusername' and password = '$mypassword'";
-      
+
       $result = $dbc->query($sql);
-		
+
       if($result->num_rows > 0) {
          session_register("myusername");
          $_SESSION['login_user'] = $myusername;
-         
+
          header("location: welcome.php");
       }else {
          $error = "Your Login Name or Password is invalid";
       }
-   }
+   }*/
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>EzMoney | LogIn</title>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-</head>
-
-<body>
-
-    <div class="container">
-
-        <form class="form-signin" action="../PHP/login.php" method="post">
-            <h2 class="form-signin-heading">Please sign in</h2>
-            <input type="hidden" name="redirect" value="<?php echo $_POST['redirect']; ?>">
-            <label for="inputEmail" class="sr-only">Email address</label>
-            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" name="username" required autofocus>
-            <label for="inputPassword" class="sr-only">Password</label>
-            <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="password" required>
-            <button type="submit" class="btn btn-lg btn-primary btn-block" />Sign in</button>
-            <br/>
-        </form>
-	<div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
-    </div>
-</body>
-</html>
