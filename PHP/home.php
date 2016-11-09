2587e2ca4952c6c99fccf2e4e62a190daf0f30ce
sbaidachni <mikedm195@gmail.com>
2016-11-09 10:41:01 -0600

    homa.php

--- PHP/home.php
+++ PHP/home.php
@@ -4,25 +4,112 @@
    $db = new DBConnection();
    $dbc = $db->Connect();
 ?>
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <title>EzMoney | <?=$name_logged?> Home</title>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+
+    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+    .row.content {height: 450px}
+
+    /* Set gray background color and 100% height */
+    .sidenav {
+      padding-top: 20px;
+      background-color: #f1f1f1;
+      height: 100%;
+    }
+
+    /* Set black background color, white text and some padding */
+    footer {
+      background-color: #555;
+      color: white;
+      padding: 15px;
+    }
+
+    /* On small screens, set height to 'auto' for sidenav and grid */
+    @media screen and (max-width: 767px) {
+      .sidenav {
+        height: auto;
+        padding: 15px;
+      }
+      .row.content {height:auto;}
+    }
+  </style>
 </head>
 <body>
-  <?php
-    $user_id = $_SESSION['login_user'];
-    $sql = "SELECT name, lastname, email, profilepic FROM Users WHERE id = $user_id";
-    $result = $dbc->query($sql);
-    print $user_id;
 
-    $row = $result->fetch_assoc();
-    print $row["name"];
-    print $row["lastname"];
-    print $row["email"];
-    print ("<img src='../RES/PP/".$row["profilepic"]."'/>");
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
-   ?>
+<div class="container-fluid text-center">
+  <div class="row content">
+    <div class="col-sm-2 sidenav">
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+    </div>
+    <div class="col-sm-8 text-left">
+      <h1>Welcome</h1>
+      <?php
+        $user_id = $_SESSION['login_user'];
+        $sql = "SELECT name, lastname, email, profilepic FROM Users WHERE id = $user_id";
+        $result = $dbc->query($sql);
+        print $user_id;
+
+        $row = $result->fetch_assoc();
+        print $row["name"];
+        print $row["lastname"];
+        print $row["email"];
+        print ("<img src='../RES/PP/".$row["profilepic"]."'/>");
+
+       ?>
+    </div>
+    <div class="col-sm-2 sidenav">
+      <div class="well">
+        <p>ADS</p>
+      </div>
+      <div class="well">
+        <p>ADS</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<footer class="container-fluid text-center">
+  <p>Footer Text</p>
+</footer>
 
 </body>
 </html>
