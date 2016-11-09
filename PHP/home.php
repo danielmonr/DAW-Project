267075f1ade25c07f148e4ecd20d8ba4b504ba81
sbaidachni <mikedm195@gmail.com>
2016-11-09 11:30:54 -0600

    home.php

--- PHP/home.php
+++ PHP/home.php
@@ -1,120 +1,90 @@
 <?php
-   include('session.php');
-   include_once('DBConnection.php');
-   $db = new DBConnection();
-   $dbc = $db->Connect();
+include('session.php');
+include_once('DBConnection.php');
+$db = new DBConnection();
+$dbc = $db->Connect();
 
-   $user_id = $_SESSION['login_user'];
-   $sql = "SELECT name, lastname, email, profilepic FROM Users WHERE id = $user_id";
-   $result = $dbc->query($sql);
-   print $user_id;
+$user_id = $_SESSION['login_user'];
+$sql = "SELECT name, lastname, email, profilepic FROM Users WHERE id = $user_id";
+$result = $dbc->query($sql);
 
-   $row = $result->fetch_assoc();
+$row = $result->fetch_assoc();
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <style>
-    /* Remove the navbar's default margin-bottom and rounded borders */
-    .navbar {
-      margin-bottom: 0;
-      border-radius: 0;
-    }
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../CSS/style.css">
 
-    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
-    .row.content {height: 450px}
-
-    /* Set gray background color and 100% height */
-    .sidenav {
-      padding-top: 20px;
-      background-color: #f1f1f1;
-      height: 100%;
-    }
-
-    /* Set black background color, white text and some padding */
-    footer {
-      background-color: #555;
-      color: white;
-      padding: 15px;
-    }
-
-    /* On small screens, set height to 'auto' for sidenav and grid */
-    @media screen and (max-width: 767px) {
-      .sidenav {
-        height: auto;
-        padding: 15px;
-      }
-      .row.content {height:auto;}
-    }
-  </style>
 </head>
 <body>
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Logo</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Logo</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-<div class="container-fluid text-center">
-  <div class="row content">
-    <div class="col-sm-2 sidenav">
-        <?php
-            print ("<img src='../RES/PP/".$row["profilepic"]."' height='50%' width='100%' />");
-        ?>
+    <div class="container-fluid text-center">
+        <div class="row content">
+            <div class="col-sm-2 sidenav">
+                <?php
+                print ("<img src='../RES/PP/".$row["profilepic"]."' height='50%' width='100%' />");
+                ?>
+            </div>
+            <div class="col-sm-8 text-left">
+                <h1>Welcome</h1>
+                <?php
+
+                print ("<div class='well'>");
+                print ("<p>" . $row["name"] . " " . $row["lastname"] . " </p>");
+                print ("</div>");
+                print ("<div class='well'>");
+                print ("<p>" . $row["email"] . " </p>");
+                print ("</div>");
+
+                ?>
+            </div>
+            <div class="col-sm-2 sidenav">
+                <div class="well">
+                    <p>Saldo</p>
+                    <p>$100,000</p>
+                </div>
+                <div class="well">
+                    <button type="button" class="btn btn-primary">Transferir</button>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="col-sm-8 text-left">
-      <h1>Welcome</h1>
-      <?php
 
-        print ("<div class='well'>");
-        print ("<p>" . $row["name"] . " " . $row["lastname"] . " </p>");
-        print ("</div>");
-        print ("<div class='well'>");
-        print ("<p>" . $row["email"] . " </p>");
-        print ("</div>");
+    <footer class="container-fluid text-center">
+        <p>Posted by: Hege Refsnes</p>
+        <p>Contact information: <a href="mailto:someone@example.com">
+            someone@example.com</a>.</p>
+        </footer>
 
-       ?>
-    </div>
-    <div class="col-sm-2 sidenav">
-      <div class="well">
-        <p>Saldo</p>
-        <p>$100,000</p>
-      </div>
-      <div class="well">
-        <button type="button" class="btn btn-primary">Transferir</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-<footer class="container-fluid text-center">
-  <p>Footer Text</p>
-</footer>
-
-</body>
-</html>
+    </body>
+    </html>
